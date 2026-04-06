d3f7d66bf9aa353f6be14fcd8aced185a60a2d52
JPDigital-Dev <jacolouw980@gmail.com>
2026-04-06 22:52:02 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 
@@ -29,4 +31,5 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
